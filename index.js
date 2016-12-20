f762dd6a2c31533815eda63badeb75a602c53795
Henrik Ruscon <henrik.dahlheim@gmail.com>
2016-12-20 22:58:28 +0100

    platform check, css for macos

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 exports.decorateConfig = (config) => {
-  return Object.assign({}, config, {
-    css: `
-      ${config.css || ''}
-      .header_windowHeader {
-        display: none;
-      }
-      .tabs_nav {
-        top: 0;
-      }
-      .terms_terms {
-        margin-top: 0;
-      }
-      .terms_termsShifted {
-        margin-top: 34px;
-      }
-    `
-  });
+    if (process.platform === 'win32') {
+        return Object.assign({}, config, {
+            css: `
+                ${config.css || ''}
+                .header_windowHeader {
+                    display: none;
+                }
+                .tabs_nav {
+                    top: 0;
+                }
+                .terms_terms {
+                    margin-top: 0;
+                }
+                .terms_termsShifted {
+                    margin-top: 34px;
+                }
+            `
+        });
+    }
+    if (process.platform === 'darwin') {
+        return Object.assign({}, config, {
+            css: `
+                ${config.css || ''}
+                .header_header {
+                    top: 0;
+                    right: 0;
+                    left: 0;
+                }
+                .tabs_borderShim {
+                    display: none;
+                }
+                .tabs_title {
+                    display: none;
+                }
+                .tabs_list {
+                    margin-left: 0;
+                }
+            `
+        });
+    }
 }
+
+// Hide window controls on macOS
+exports.decorateBrowserOptions = defaults => Object.assign({}, defaults, {
+    titleBarStyle: '',
+    transparent: true,
+    frame: false
+})
