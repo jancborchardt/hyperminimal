0d1d107f9e57b9f95f7104db5126d78a28491058
Henrik Ruscon <henrik.dahlheim@gmail.com>
2016-12-21 23:50:27 +0100

    2 tab spaces instead of 4, updated css method

--- index.js
+++ index.js
@@ -1,49 +1,46 @@
 exports.decorateConfig = (config) => {
-    if (process.platform === 'win32') {
-        return Object.assign({}, config, {
-            css: `
-                ${config.css || ''}
-                .header_windowHeader {
-                    display: none;
-                }
-                .tabs_nav {
-                    top: 0;
-                }
-                .terms_terms {
-                    margin-top: 0;
-                }
-                .terms_termsShifted {
-                    margin-top: 34px;
-                }
-            `
-        });
+  var defaultCSS = `
+    .header_header {
+      top: 0;
+      right: 0;
+      left: 0;
     }
-    if (process.platform === 'darwin') {
-        return Object.assign({}, config, {
-            css: `
-                ${config.css || ''}
-                .header_header {
-                    top: 0;
-                    right: 0;
-                    left: 0;
-                }
-                .tabs_borderShim {
-                    display: none;
-                }
-                .tabs_title {
-                    display: none;
-                }
-                .tabs_list {
-                    margin-left: 0;
-                }
-            `
-        });
+    .tabs_borderShim {
+      display: none;
     }
+    .tabs_title {
+      display: none;
+    }
+    .tabs_list {
+      margin-left: 0;
+    }
+  `
+  var windowsCSS = `
+    .header_windowHeader {
+      display: none;
+    }
+    .tabs_nav {
+      top: 0;
+    }
+    .terms_terms {
+      margin-top: 0;
+    }
+    .terms_termsShifted {
+      margin-top: 34px;
+    }
+  `
+
+  return Object.assign({}, config, {
+  css: `
+      ${config.css || ''}
+      ${process.platform === 'win32' ? windowsCSS : defaultCSS}
+    `
+  });
 }
 
 // Hide window controls on macOS
 exports.decorateBrowserOptions = defaults => Object.assign({}, defaults, {
-    titleBarStyle: '',
-    transparent: true,
-    frame: false
+  titleBarStyle: '',
+  transparent: true,
+  frame: false
 })
