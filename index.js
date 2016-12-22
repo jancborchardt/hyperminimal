3b3ec54992b2b8c87e7852c5805b4c6bb4f63182
Riku Murai <riku.murai15@imperial.ac.uk>
2016-12-22 20:52:16 +0100

    Removes the gap if there is only one tab open

--- index.js
+++ index.js
@@ -44,3 +44,19 @@ exports.decorateBrowserOptions = defaults => Object.assign({}, defaults, {
   transparent: true,
   frame: false
 })
+
+// Removes the redundant space on mac if there is only one tab
+exports.getTabsProps = (parentProps, props) => {
+  if (process.platform === 'darwin') {
+    var classTermsList = document.getElementsByClassName('terms_terms')
+    if (classTermsList.length > 0) {
+      var classTerm = classTermsList[0]
+      if (props.tabs.length <= 1) {
+        classTerm.setAttribute("style", "margin-top: 0")
+      } else {
+        classTerm.setAttribute("style", "")
+      }
+    }
+  }
+  return Object.assign({}, parentProps, props)
+}
